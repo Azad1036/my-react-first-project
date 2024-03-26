cf9138c11fcd3ed09040d0543a940444555665c7
unknown <mohammadazadhossain2024@gmail.com>
2024-03-26 14:48:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      
+      <h1 className='text-2xl bg-red-600'>Recat Knowloge Cafe</h1>
+      
+     
+    </>
+  )
+}
+
+export default App

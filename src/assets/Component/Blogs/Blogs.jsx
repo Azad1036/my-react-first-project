30f3cb3820f72372161e61262f5d58541a50bfba
unknown <mohammadazadhossain2024@gmail.com>
2024-03-26 17:01:08 +0600

    added author image and all post

--- src/assets/Component/Blogs/Blogs.jsx
+++ src/assets/Component/Blogs/Blogs.jsx
@@ -1,10 +1,24 @@
 import PropTypes from 'prop-types';
 
 const Blogs = ({ blog }) => {
-    const {title} = blog
+    const {title, cover, author_image, author, posted_date,reading_time} = blog
     return (
         <div>
-            <h1>Name {blog.title} </h1>
+            <img className='w-[845px]' src={cover} alt="" />
+            <div className='flex justify-between items-center'>
+                <div className='flex items-center'>
+                    <img className='w-16' src={author_image} alt="" />
+                    <div>
+                        <h3 className='font-bold'>{author}</h3>
+                        <p><small>{posted_date}</small></p>
+                    </div>
+                </div>
+                        <p>{reading_time}</p>
+                <div>
+                </div>
+
+            </div>
+            <h1 className='text-4xl font-bold'>Name {title} </h1>
         </div>
     );
 };
